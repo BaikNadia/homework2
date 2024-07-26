00801b795d12290b7ea98a783a27426df3758ce7
BaikNadia <140077865+BaikNadia@users.noreply.github.com>
2024-07-26 19:31:50 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+home_work = 12
+
+time_spent = 1.5
+
+course_name = 'Python'
+
+time_spent_one = time_spent / home_work
+
+print(course_name,'всего задач',home_work,'затрачено часов', time_spent,'среднее время выполнения', time_spent_one)
+
+
+
